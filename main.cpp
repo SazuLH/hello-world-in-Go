c3b54841b5deed1f9713e8ad5ae60913958a173f
Saul Luciano <saulluciano13@gmail.com>
2020-11-04 22:22:41 -0400

    primer commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int a;
+int b;
+int sum;
+cout<<"introdusca un numero\n";
+cin>>a;
+cout<<"introdusca otro numero\n";
+cin>>b;
+sum=a*b;
+cout<<"el resultado es:\n"<<sum<<endl;
+cin>>sum;
+
+return 0;
+
+}
+
+
